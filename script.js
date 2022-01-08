5afcadf44d927cbbb8b07891ba92fb9fb9cdb386
Michael Kipp <michaelkipp@outlook.com>
2022-01-08 09:03:46 -0600

    Rebuild

---
+++ script.js
@@ -0,0 +1,32 @@
+var $grid = $('.grid').isotope({
+  // options
+  itemSelector: '.gallery-item'
+});
+
+var $buttonGroup = $('.filters');
+$buttonGroup.on( 'click', 'li', function( event ) {
+  $buttonGroup.find('.is-checked').removeClass('is-checked');
+  var $button = $( event.currentTarget );
+  $button.addClass('is-checked');
+  var filterValue = $button.attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+// filter .childhood items
+$grid.isotope({ filter: '.childhood' });
+
+// filter .doodle items
+$grid.isotope({ filter: '.doodle’' });
+
+// filter .college items
+$grid.isotope({ filter: '.college' });
+
+// filter .video items
+$grid.isotope({ filter: '.video' });
+
+// show all items
+$grid.isotope({ filter: '*' });
+
+Fancybox.bind('[data-fancybox="single"]', {
+  groupAttr: false
+});
